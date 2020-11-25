21d59f282726be15d0b83972e3ff180380956f35
flavioro <flavioro@gmail.com>
2020-11-25 16:10:05 -0300

    Component SEO wiht title, description etc

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -18,17 +18,18 @@ export default function SEO({
   const pageTitle = `${title} ${!shouldExcludeTitleSuffix ? '| DevCommerce' : ''}`
   const pageImage = image ? `${process.env.SITE_URL}/${image}` : null;
 
-  { description && <meta name='description' content={description}/>}
-  { pageImage && <meta name='image' content={pageImage}/>}
   
-  { !shouldIndexPage && <meta name="robots" content="noindex, nofollow"/>}
-
-  // link to others meta tags 
-  // https://gist.github.com/diego3g/fa876d6f954390c656e490e538e8953f#file-metatags-html-L17
-
   return (
     <Head>
       <title>{pageTitle}</title>
+    
+      { description && <meta name='description' content={description}/>}
+      { pageImage && <meta name='image' content={pageImage}/>}
+      
+      { !shouldIndexPage && <meta name="robots" content="noindex, nofollow"/>}
+    
+      // link to others meta tags 
+      // https://gist.github.com/diego3g/fa876d6f954390c656e490e538e8953f#file-metatags-html-L17
     </Head>
   )
 }
